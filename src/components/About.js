d35c9f5566976092c116b422010544eb4ff5fbef
Mr-Emerald-Wolf <shivamsharma2002@gmail.com>
2022-01-29 15:56:43 +0530

    Spelling mistake

--- src/components/About.js
+++ src/components/About.js
@@ -11,7 +11,7 @@ export default function About() {
                                 My name is Shivam.
                             </h1>
                             <p className='text-sky-800 dark:text-sky-200 lg:w-2/3 mx-auto text-left md:text-xl p-1'>I am a full stack developer based in Delhi. I am a student currently pursuing my B.Tech in CSE from VIT university.</p>
-                            <p className='text-sky-800 dark:text-sky-200 lg:w-2/3 mx-auto text-left md:text-xl p-1'>Coding has always been my passion and I would love to explore new areas like ML {"&"} Competetive Programming.</p>
+                            <p className='text-sky-800 dark:text-sky-200 lg:w-2/3 mx-auto text-left md:text-xl p-1'>Coding has always been my passion and I would love to explore new areas like ML {"&"} Competitive Programming.</p>
                             <p className='text-sky-800 dark:text-sky-200 lg:w-2/3 mx-auto text-left md:text-xl p-1'>I love to explore new types of Speciality Coffee, play open-world games and watch Trash Taste.</p>
                             <p className='text-sky-800 dark:text-sky-200 lg:w-2/3 mx-auto text-left md:text-xl p-1'>I create web applications using ReactJS and Tailwind. </p>
                         </div>
