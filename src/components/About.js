e68841bb9c59a3180e6cf95b8456318e74c163f5
Mr-Emerald-Wolf <shivamsharma2002@gmail.com>
2022-02-13 10:43:47 +0530

    Minor changes

--- src/components/About.js
+++ src/components/About.js
@@ -10,14 +10,14 @@ export default function About() {
                             <h1 className="sm:text-4xl text-3xl font-medium title-font m-4 md:mb-2 md:pb-4 text-sky-900 dark:text-sky-300" >
                                 My name is Shivam.
                             </h1>
-                            <p className='text-sky-800 dark:text-sky-200 lg:w-2/3 mx-auto text-left md:text-xl p-1'>I am a full stack developer based in Delhi. I am a student currently pursuing my B.Tech in CSE from VIT university.</p>
+                            <p className='text-sky-800 dark:text-sky-200 lg:w-2/3 mx-auto text-left md:text-xl p-1'>I am a Front End developer based in Delhi. I am a student currently pursuing my B.Tech in CSE from VIT university.</p>
                             <p className='text-sky-800 dark:text-sky-200 lg:w-2/3 mx-auto text-left md:text-xl p-1'>Coding has always been my passion and I would love to explore new areas like ML {"&"} Competitive Programming.</p>
                             <p className='text-sky-800 dark:text-sky-200 lg:w-2/3 mx-auto text-left md:text-xl p-1'>I love to explore new types of Speciality Coffee, play open-world games and watch Trash Taste.</p>
                             <p className='text-sky-800 dark:text-sky-200 lg:w-2/3 mx-auto text-left md:text-xl p-1'>I create web applications using ReactJS and Tailwind. </p>
                         </div>
                     </div>
                     <div className="p-1 md:w-1/2">
-                        <img className='mx-auto w-2/3 md:w-2/3 lg:w-1/2' src="./img/meme.jpg" alt="Profile" />
+                        <img className='mx-auto w-2/3 md:w-2/3 lg:w-1/2' src="./img/profile.jpg" alt="Profile" />
                     </div>
                 </div>
             </section>
