ac79ac3ed3c6391b772b8c8e7071b97d6a58db61
Mr-Emerald-Wolf <shivamsharma2002@gmail.com>
2023-02-11 16:11:38 +0530

    update: about me

--- src/components/About.js
+++ src/components/About.js
@@ -10,10 +10,10 @@ export default function About() {
                             <h1 className="sm:text-4xl text-3xl font-medium title-font m-4 md:mb-2 md:pb-4 text-sky-900 dark:text-sky-300" >
                                 My name is Shivam.
                             </h1>
-                            <p className='text-sky-800 dark:text-sky-200 lg:w-2/3 mx-auto text-left md:text-xl p-1'>I am a Front End developer based in Delhi. I am a student currently pursuing my B.Tech in CSE from VIT university.</p>
+                            <p className='text-sky-800 dark:text-sky-200 lg:w-2/3 mx-auto text-left md:text-xl p-1'>I am a Full Stack developer based in Delhi. I am a student currently pursuing my B.Tech in CSE from VIT university.</p>
                             <p className='text-sky-800 dark:text-sky-200 lg:w-2/3 mx-auto text-left md:text-xl p-1'>Coding has always been my passion and I would love to explore new areas like ML {"&"} Competitive Programming.</p>
                             <p className='text-sky-800 dark:text-sky-200 lg:w-2/3 mx-auto text-left md:text-xl p-1'>I love to explore new types of Speciality Coffee, play open-world games and watch Trash Taste.</p>
-                            <p className='text-sky-800 dark:text-sky-200 lg:w-2/3 mx-auto text-left md:text-xl p-1'>Right now I am currently working on projects based on React and C++.</p>
+                            <p className='text-sky-800 dark:text-sky-200 lg:w-2/3 mx-auto text-left md:text-xl p-1'>Right now I am currently working on projects based on Go and ExpressJS.</p>
                         </div>
                     </div>
                     <div className="p-1 md:w-1/2">
